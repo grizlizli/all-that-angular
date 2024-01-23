f62d095c7bcb02a1c782516119f6226b075d1103
Miloš Krstić <mrkrsticmilos@gmail.com>
2024-01-23 09:50:46 +0100

    code style change

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -9,17 +9,12 @@ import { Todo } from '../../interfaces/todo.interface';
   styleUrl: './todo-item.component.scss'
 })
 export class TodoItemComponent implements Todo {
-  @Input({required: true})
-  id!: number;
-  @Input({required: true})
-  text!: string;
-  @Input({required: true})
-  completed!: boolean;
+  @Input({required: true}) id!: number;
+  @Input({required: true}) text!: string;
+  @Input({required: true}) completed!: boolean;
 
-  @Output()
-  completeChange: EventEmitter<number> = new EventEmitter<number>();
-  @Output()
-  delete: EventEmitter<number> = new EventEmitter<number>();
+  @Output() completeChange: EventEmitter<number> = new EventEmitter<number>();
+  @Output() delete: EventEmitter<number> = new EventEmitter<number>();
 
   handleCompletitionChange(id: number): void {
     this.completeChange.emit(id);
