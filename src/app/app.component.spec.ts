28fe64b2dbaab3348363475e9f0a7bea8bd263e4
grizlizli <mkrstic@capaciteam.com>
2024-03-18 00:37:47 +0100

    app component test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should render title 'All That Angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('All That Angular');
+  it(`should render 'All That Angular' as title`, () => {
+    const element = fixture.nativeElement as HTMLElement;
+    expect(element.querySelector('h1')?.textContent).toContain('All That Angular');
+  });
+
+  it(`should render 'All That Angular' as title [By example]`, () => {
+    const titleElement: any = fixture.debugElement.query(
+      By.css(
+        '[data-testid="mk-app-title"]'
+      )
+    );
+    expect(titleElement.nativeElement.textContent).toContain('All That Angular');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { TooltipDirective } from './components/tooltip/tooltip.directive';
 import { TodosListComponent } from './components/todos-list/todos-list.component';
 
 @Component({
   selector: 'mk-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TooltipDirective, TodosListComponent],
+  imports: [CommonModule, TooltipDirective, TodosListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
