aa07aa58684c990671fa413319e5a60ed50d0d27
grizlizli <mkrstic@capaciteam.com>
2024-04-21 12:05:30 +0200

    loading products

--- src/app/components/loading-products-list/loading-products-list.component.ts
+++ src/app/components/loading-products-list/loading-products-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'mk-loading-products-list',
   standalone: true,
   imports: [],
   templateUrl: './loading-products-list.component.html',
-  styleUrl: './loading-products-list.component.scss'
+  styleUrl: './loading-products-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoadingProductsListComponent {
-
+  readonly items = new Array<void>(20)
 }
