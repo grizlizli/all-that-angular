6b78e8f1adea0214b6b26e70d0f109fba3260e87
grizlizli <mkrstic@capaciteam.com>
2024-05-04 10:19:29 +0200

    retry config

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, retry } from 'rxjs';
+import { Observable, retry, RetryConfig } from 'rxjs';
 import { API_ENDPOINT_URL } from '../providers/api-endpoint-url.provider';
 import { API_ENDPOINT_CONFIG, ApiEndpointConfig } from '../providers/api-endpoint-config.provider';
+
+const retryConfig: RetryConfig = {
+  count: 3,
+  delay: 3000
+}
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +20,11 @@ export class ApiService {
   private readonly endpoint: string = `${this.apiEndpointConfig.endpoint}/${this.apiEndpointConfig.prefix}/v${this.apiEndpointConfig.version}`;
 
   get<T = unknown>(route: string, params: any = {}): Observable<T> {
-    return this.http.get<T>(`${this.endpointUrl}/${route}`, { params }).pipe(retry(3));
+    return this.http.get<T>(`${this.endpointUrl}/${route}`, { params }).pipe(retry(retryConfig));
   }
   
   post<T = unknown>(route: string, body: unknown): Observable<T> {
-    return this.http.post<T>(`${this.endpointUrl}/${route}`, body).pipe(retry(3));
+    return this.http.post<T>(`${this.endpointUrl}/${route}`, body).pipe(retry(retryConfig));
   }
 
   put<T = unknown>(route: string, body: unknown) {
